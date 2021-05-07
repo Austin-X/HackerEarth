bd14a9b4187bd313735d4e9fa87e4358c9b94b89
Austin-X <32133198+Austin-X@users.noreply.github.com>
2021-05-06 20:55:01 -0400

    Create Conject-It!.java

---
+++ Basics of Input and Output/Conject-It!.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class ConjectIt {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int T = readInt();
+		for (int t = 0; t < T; t ++) {
+			@SuppressWarnings("unused")
+			String s = readLine();
+			System.out.println("YES");
+		}
+	}
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) 
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine() throws IOException {
+		return br.readLine().trim();
+	}
+}
